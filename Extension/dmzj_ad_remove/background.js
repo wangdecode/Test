b263ba05a35b4d6b2d971bfa0719a94128646e4f
wangdecode <49616194+wangdecode@users.noreply.github.com>
2020-06-09 23:32:08 +0800

    Add files via upload

---
+++ Extension/dmzj_ad_remove/background.js
@@ -0,0 +1,11 @@
+﻿chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         //console.log(details.url);
+         return {cancel: true};
+    },
+    {
+        urls:["*://*.dmzj.com/js/ad*", "*://*.dmzj.com/css/share/share_css.css"],
+        types: ["script", "stylesheet"]
+    },
+    ["blocking"]
+);
