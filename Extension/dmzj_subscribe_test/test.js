db39c5b5550ed30fa5957fc7403ebd9ec6db1045
wangdecode <49616194+wangdecode@users.noreply.github.com>
2020-06-09 23:29:38 +0800

    Add files via upload

---
+++ Extension/dmzj_subscribe_test/test.js
@@ -0,0 +1,7 @@
+﻿//add js
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('main.js');
+s.onload = function() {
+    this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(s);
