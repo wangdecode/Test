cd9750102ff61d63f0fdf5ec84023957b387aeb6
wangdecode <49616194+wangdecode@users.noreply.github.com>
2022-02-18 12:32:02 +0800

    Update content.js

--- Extension/Crx_pic_download/content.js
+++ Extension/Crx_pic_download/content.js
@@ -10,7 +10,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         chrome.runtime.sendMessage({
             "message": "download", "urls": urls, "path": path});
     }
-    
+    if(request.cmd == 'pic_remove_space') {
+        pic_remove_space();
+    }
     if(request.cmd == 'getnum') {
         let num = pic_explain().length;
         chrome.runtime.sendMessage({
@@ -20,13 +22,28 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 //图片解析
 function pic_explain() {
+    //let main = document.getElementById("");
     let pics = document.getElementsByTagName("img");
     if (pics.length == 0) return;
     
     let pics_url = new Array();
+    let index = 0;
     for (i=0;i<pics.length;i++) {
-        pics_url[i]=pics[i].src;
+        //判断图片高, 宽
+        if (pics[i].height < 300 || pics[i].width < 100) continue;
+        pics_url[index]=pics[i].src;
+        index++;
     }
     
     return pics_url;
+}
+
+//图片去除间隔
+function pic_remove_space() {
+    if(window.location.hostname.indexOf("cnanjie.com") == -1 )
+        return;
+    let a=document.getElementsByClassName("img_info");
+    for(i=a.length-1;i>=0;i--) {
+        a[i].remove();
+    }
 }
