e41431e27dfb3f80e957aa7fa1fb12e40f2dbe9d
wangdecode <49616194+wangdecode@users.noreply.github.com>
2023-03-18 10:00:18 +0800

    Update main.js

--- Extension/dmzj_subscribe_test/main.js
+++ Extension/dmzj_subscribe_test/main.js
@@ -1,35 +1,21 @@
-﻿function my_subscribe(type) {
+function my_subscribe(type) {
     if(!isLogin){
         console.log("用户未登录");
         return;
     }
     if (type){
-        my_subscribe_add(g_comic_id, 0, this);
+        my_subscribe_add();
     }else{
-        my_subscribe_remove(g_comic_id, 0, this);
+        my_subscribe_remove();
     }
 }
 
-function my_subscribe_add(subId,sub_type,obj) {
-    var url = ajax_url+"api/subscribe/add";
-    T.ajaxJsonp(url,{sub_id:subId,uid:userId,sub_type:sub_type}, function (data) {
-        if(data.result==1000){
-            console.log("订阅成功");
-        }else{
-            return 0;
-        }
-    })
+function my_subscribe_add() {
+    
 }
 
-function my_subscribe_remove(subId,sub_type,obj){
-    var url = ajax_url+"api/subscribe/del";
-    T.ajaxJsonp(url,{sub_id:subId,uid:userId,sub_type:sub_type}, function (data) {
-        if(data.result==1000){
-            console.log("取消订阅成功");
-        }else{
-            return 0;
-        }
-    })
+function my_subscribe_remove(){
+    
 }
 
 var html ="<a>  </a><button id=\"btn1\" onclick=\"my_subscribe(true)\">订阅</button><a> </a><button id=\"btn2\" onclick=\"my_subscribe(false)\">取订</button>";
