08dda446e5eabfb12ea497cf9614fd2a31866a25
wangdecode <49616194+wangdecode@users.noreply.github.com>
2024-07-22 21:48:00 +0800

    Add files via upload

---
+++ modi_ocr_test/modi_ocr/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace modi_ocr
+{
+    public partial class Form2 : Form
+    {
+        Point offset;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Left != e.Button) return;
+            Point cur = this.PointToScreen(e.Location);
+            offset = new Point(cur.X - this.Left, cur.Y - this.Top);
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Left != e.Button) return;
+            Point cur = MousePosition; this.Location = new Point(cur.X - offset.X, cur.Y - offset.Y);
+        }
+    }
+}
